dd8ce90675047b1a75a9340a711a8209f2cac826
Oleksandra Parkhomenko <alex.vladimirovnaa@gmail.com>
2023-02-03 00:37:14 +0200

    додає скрипт до 2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,14 @@
 import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let CURRENT_TIME_KEY = 'videoplayer-current-time'
+
+const onPlay = function(data){
+    localStorage.setItem(CURRENT_TIME_KEY, data.seconds)
+    // console.log(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)))
+}
+
+player.on('timeupdate',throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY) || 0);
